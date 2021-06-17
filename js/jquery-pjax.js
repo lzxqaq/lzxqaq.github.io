d72c9151a4b4567cce3f3b26cd73b02f56330908
lzxqaq <lzxqaq@163.com>
2021-06-18 03:17:44 +0800

    update

--- js/jquery-pjax.js
+++ js/jquery-pjax.js
@@ -1,21 +1,25 @@
-$('a').bind('click',function(){
-    $.ajax({
-        type:'GET',
-        url:this.href,
-        success:function(data){
-            console.log("success!");
-            $('#main').html(data);//pjax-content为主体部分
-        }
+$(function(){
+ 
+    $('a').bind('click',function(){
+        console.log("click!");
+        $.ajax({
+            type:'GET',
+            url:this.href,
+            success:function(data){
+                console.log("success!");
+                $('#main').html(data);//pjax-content为主体部分
+            }
+        });
+        window.history.pushState({url:this.href},null,this.href);
+        return false;
     });
-    window.history.pushState({url:this.href},null,this.href);
-    return false;
-});
-window.addEventListener("popstate", function() {
-    $.ajax({
-        type:'GET',
-        url:location.href,
-        success:function(data){
-            $('#main').html(data);
-        }
-    });										
-});
+    window.addEventListener("popstate", function() {
+        $.ajax({
+            type:'GET',
+            url:location.href,
+            success:function(data){
+                $('#main').html(data);
+            }
+        });										
+    });  
+ });
