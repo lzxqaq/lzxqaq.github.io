85a0be77d550a841870fb1276d7daf354c9678ca
lzxqaq <lzxqaq@163.com>
2021-06-18 08:04:24 +0800

    update

---
+++ js/pjax.config.js
@@ -0,0 +1,31 @@
+$(function(){
+
+	$.pjax({
+		selector: 'a',
+		container: '#pjax-content', //被替换的容器
+		_container: '#pjax-content', //要替换的容器
+		show: 'fade',  //展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
+		cache: true,  //是否使用缓存
+		storage: true,  //是否使用本地存储
+		titleSuffix: '', //标题后缀
+		filter: function(){},
+		callback: function(){}
+	});
+  
+	if (!!window.localStorage) {
+		for (var key in localStorage) {
+			try {
+				if ((key.split("_") || [""])[0] === "pjax") {
+					var item = localStorage.getItem(key);
+					if (item) {
+						item = JSON.parse(item);
+						if ((parseInt(item.time) + 600 * 1000) <= new Date * 1) {
+							localStorage.removeItem(key)
+						}
+					}
+				}
+				} catch (e) { }
+		}
+	}
+  
+});
---
+++ js/pjax.test.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('body').delegate('a', 'click', function (event) {
+        event.preventDefault();
+        $.ajax({
+            type:'GET',
+            url:this.href,
+            success:function(data){
+                var txt = document.createElement("div");
+                txt.innerHTML = data;
+                data = $(txt).find("#pjax-content").html();
+                $('#pjax-content').html(data);
+            }
+        });
+      //改变地址栏并添加历史记录
+        window.history.pushState({url:this.href},null,this.href);
+    });
+    window.addEventListener("popstate", function() {
+        $.ajax({
+            type:'GET',
+            url:location.href,
+            success:function(data){
+                $('#pjax-content').html(data);
+            }
+        });										
+    });
+});
